10d09b0331c7dfdc58a8879ae6e5418781f130c5
Ic3Cr34m <21297015+IceCr34m@users.noreply.github.com>
2022-12-10 19:41:52 +0100

    Update background.js
    fruther removed analytics.

--- src/background.js
+++ src/background.js
@@ -13,10 +13,8 @@
 // limitations under the License.
 
 chrome.action.onClicked.addListener((tab) => {
-  chrome.storage.sync.get({ optOutAnalytics: false }, (results) => {
-    const files = results.optOutAnalytics
-      ? ["script.js"]
-      : ["script.js", "ga.js"];
+  chrome.storage.sync.get({ optOutAnalytics: true }, (results) => {
+    const files = ["script.js"];
     chrome.scripting.executeScript({
       target: { tabId: tab.id, allFrames: true },
       world: "MAIN",
